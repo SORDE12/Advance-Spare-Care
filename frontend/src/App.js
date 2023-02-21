6ff3f89c5482e5b5c5e5d50e24d1b869640a9480
SORDE12 <sordeomkar2564@gmail.com>
2023-02-21 14:26:52 +0530

    added

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import AllRoutes from './Pages/AllRoutes';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <AllRoutes/>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Pages/AllRoutes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllRoutes = () => {
+  return (
+    <div>AllRoutes</div>
+  )
+}
+
+export default AllRoutes
