417ffe53e9bacb85367c97981a67a991501ef9c9
bhavnesh1811 <erbhavnesharora@gmail.com>
2023-02-23 02:23:25 +0530

    updated get routes in product

--- backend/Routes/Product.Route.js
+++ backend/Routes/Product.Route.js
@@ -1,61 +1,62 @@
 const express = require("express");
 const { authentication } = require("../Middlewares/admin.authentication");
-const { userAuthentication } = require("../Middlewares/user.authentication");
-const { CartModel } = require("../Models/Cart.Model");
 const { ProductModel } = require("../Models/Product.Model");
 
 const ProductRouter = express.Router();
 
-ProductRouter.post("/add",authentication, async (req, res) => {
+ProductRouter.post("/add", authentication, async (req, res) => {
   try {
     const product = new ProductModel(req.body);
     await product.save();
-    res.status(200).send({ msg: "Product Added",product });
+    res.status(200).send({ msg: "Product Added" });
   } catch (e) {
-    res.status(200).send({ msg: "Product Not Added", err: e.message });
+    res.status(404).send({ msg: "Product Not Added", err: e.message });
   }
 });
-
-ProductRouter.post("/:id",userAuthentication,authentication,async(req,res)=>{
-  const ID=req.params.id;
-
-  try{
-    const cart=await CartModel.find();
-    const product=await ProductModel.findById({_id:ID});
-    cart.push(product);
-    await cart.save();
-    res.send({msg:"Added to Cart",cart})
-  }catch(e){
-    res.send({error:e.message})
-  }
-
-})
 
 ProductRouter.get("/", async (req, res) => {
-
-  const products = await ProductModel.find();
-  res.status(200).send(products);
+  const { category, min, max, category1 } = req.query;
+  console.log(category1);
+  if (category && category1) {
+    const products = await ProductModel.find({
+      $or: [ {category: { $regex: `${category}`, $options: "i" }}, {category: { $regex: `${category1}`, $options: "i" }}],
+    });
+    res.status(200).send(products);
+  } else if (category) {
+    const products = await ProductModel.find({
+      category: { $regex: `${category}`, $options: "i" },
+    });
+    res.status(200).send(products);
+  } else if (min && max) {
+    const products = await ProductModel.find({
+      $and: [{ price: { $gte: min } }, { price: { $lte: max } }],
+    });
+    res.status(200).send(products);
+  } else {
+    const products = await ProductModel.find();
+    res.status(200).send(products);
+  }
 });
 
-ProductRouter.patch("/update/:id",authentication, async (req, res) => {
+ProductRouter.patch("/update/:id", authentication, async (req, res) => {
   const ID = req.params.id;
   const payload = req.body;
   try {
     await ProductModel.findByIdAndUpdate({ _id: ID }, payload);
     res.status(200).send({ msg: "Product Updated" });
   } catch (e) {
-    res.status(200).send({ msg: "Product Not Updated", err: e.message });
+    res.status(404).send({ msg: "Product Not Updated", err: e.message });
   }
 });
 
-ProductRouter.delete("/delete/:id",authentication, async (req, res) => {
+ProductRouter.delete("/delete/:id", authentication, async (req, res) => {
   const ID = req.params.id;
 
   try {
     await ProductModel.findByIdAndDelete({ _id: ID });
     res.status(200).send({ msg: "Product Deleted" });
   } catch (e) {
-    res.status(200).send({ msg: "Product Not Deleted", err: e.message });
+    res.status(404).send({ msg: "Product Not Deleted", err: e.message });
   }
 });
 
