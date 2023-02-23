d208bb111a934f7266b50817152245af50d0ab1b
bhavnesh1811 <erbhavnesharora@gmail.com>
2023-02-24 01:46:38 +0530

    added login functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,16 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import AllRoutes from "./Pages/AllRoutes";
 import Footer from "./Components/Footer";
-import Register from "./Pages/Register";
-import Login from "./Pages/Login";
-import Homepage from "./Pages/Homepage";
+// import Register from "./Pages/Register";
+// import Login from "./Pages/Login";
+// import Homepage from "./Pages/Homepage";
 
 function App() {
   return (
     <div className="App">
      < Navbar/>
-      <Admin />
-      <AdminRoutes />
+      {/* <Admin /> */}
+      {/* <AdminRoutes /> */}
       <AllRoutes/>
       <Footer />
     </div>
--- frontend/src/Pages/AllRoutes.jsx
+++ frontend/src/Pages/AllRoutes.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {  Route, Routes } from 'react-router-dom'
+import Homepage from './Homepage'
+import Login from './Login'
 
 const AllRoutes = () => {
   return (
-    <div>AllRoutes</div>
+    <Routes>
+      <Route path='/' element={<Homepage/>}/>
+      <Route path='/login' element={<Login/>}/>
+    </Routes>
   )
 }
 
