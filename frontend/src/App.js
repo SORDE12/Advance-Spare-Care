3e36590c5dec647e8a71ecb59227a3ec823a26ec
shivamsherje <sherjes49@gmail.com>
2023-02-23 22:37:53 +0530

    day3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,14 @@ import AllRoutes from "./Pages/AllRoutes";
 import Footer from "./Components/Footer";
 import Register from "./Pages/Register";
 import Login from "./Pages/Login";
-
+import Homepage from "./Pages/Homepage";
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar /> */}
 
-      <AllRoutes />
-
+      {/* <AllRoutes /> */}
+      <Homepage />
       <Footer />
     </div>
   );
